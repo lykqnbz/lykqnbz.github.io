235eef5aea25b44276f023d145d9f3904013cfdf
lykqnbz <hongjianpiaoxiang@163.com>
2020-03-10 16:53:52 +0800

    debug

--- mySey/pro/anydoor/README.md
+++ mySey/pro/anydoor/README.md
@@ -1,4 +1,22 @@
 
 ##### nodeJs 静态资源服务器Ptc :shaved_ice:.
 
-#### Xi
+#### 安装
+
+```
+npm i -g xiaogebbb
+
+```
+
+## 使用方式
+
+```
+xiaogebbb # 把当前文件夹座位静态资源服务器根目录
+
+xiaogebbb -p 8080 # 设置端口号为8080
+
+xiaogebbb -h localhost # 设置 host 为localhost
+
+xiaogebbb -d /usr #设置根目录为/usr
+
+```
--- mySey/pro/anydoor/src/config/defaultConfig.js
+++ mySey/pro/anydoor/src/config/defaultConfig.js
@@ -2,5 +2,12 @@ module.exports = {
   root: process.cwd(),
   hostname: '127.0.0.1',
   port: 9527,
-  compress: /\.(html|js|js|md)/
+  compress: /\.(html|js|js|md)/,
+  cache: {
+    maxAge: 600,
+    expires: true,
+    cacheControl: true,
+    lastModified: true,
+    etag: true
+  }
 }
